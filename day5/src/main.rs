43f45961de296e39e48ca65b36d09e6460aeefe9
James Chacon <chacon.james@gmail.com>
2022-12-04 21:34:13 -0800

    day5 part 1

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -11,7 +11,36 @@ fn main() -> Result<()> {
     let file = File::open(filename)?;
     let lines: Vec<String> = io::BufReader::new(file).lines().flatten().collect();
 
+    let mut stacks = vec![
+        vec!['D', 'T', 'W', 'F', 'J', 'S', 'H', 'N'],
+        vec!['H', 'R', 'P', 'Q', 'T', 'N', 'B', 'G'],
+        vec!['L', 'Q', 'V'],
+        vec!['N', 'B', 'S', 'W', 'R', 'Q'],
+        vec!['N', 'D', 'F', 'T', 'V', 'M', 'B'],
+        vec!['M', 'D', 'B', 'V', 'H', 'T', 'R'],
+        vec!['D', 'B', 'Q', 'J'],
+        vec!['D', 'N', 'J', 'V', 'R', 'Z', 'H', 'Q'],
+        vec!['B', 'N', 'H', 'M', 'S'],
+    ];
     for (line_num, l) in lines.iter().enumerate() {
+        let parts: Vec<&str> = l.split_whitespace().collect();
+        if parts.len() == 0 || parts[0] != "move" {
+            println!("skipping - {l}");
+            continue;
+        }
+        assert!(parts.len() == 6, "{} - bad line - {l}", line_num + 1);
+        let num = u32::from_str_radix(parts[1], 10)?;
+        let src = usize::from_str_radix(parts[3], 10)?;
+        let dest = usize::from_str_radix(parts[5], 10)?;
+
+        for _ in 0..num {
+            let v = stacks[src - 1].pop().unwrap();
+            //println!("{} - {l} - {i} {src} {dest}", line_num + 1);
+            stacks[dest - 1].push(v);
+        }
+    }
+    for mut v in stacks {
+        println!("top - {}", v.pop().unwrap());
     }
     Ok(())
 }
