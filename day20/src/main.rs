d0217b907a48eed5a75749083a39f33a93d908a0
James Chacon <chacon.james@gmail.com>
2022-12-07 12:17:45 -0800

    skeleton for remaining days

---
+++ day20/src/main.rs
@@ -0,0 +1,19 @@
+//! day20 advent 2022
+use color_eyre::eyre::Result;
+use slab_tree::tree::TreeBuilder;
+use std::collections::HashMap;
+use std::fs::File;
+use std::io;
+use std::io::BufRead;
+use std::path::Path;
+
+fn main() -> Result<()> {
+    let filename = Path::new(env!("CARGO_MANIFEST_DIR")).join("input.txt");
+    let file = File::open(filename)?;
+    let lines: Vec<String> = io::BufReader::new(file).lines().flatten().collect();
+
+    for (line_num, line) in lines.iter().enumerate() {
+    }
+
+    Ok(())
+}
