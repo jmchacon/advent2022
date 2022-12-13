754c1fc65725001dc134aa4aac7936dfbb54f704
James Chacon <chacon.james@gmail.com>
2022-12-13 18:24:09 +0000

    day13 part1.
    Needed help from another solution to figure out my bugs.
    
    Once turned into equal/less/greater things worked.

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -1,18 +1,25 @@
 //! day13 advent 2022
 use clap::Parser;
 use color_eyre::eyre::Result;
-use slab_tree::tree::TreeBuilder;
-use std::collections::HashMap;
+use std::cmp::Ordering;
 use std::fs::File;
 use std::io;
 use std::io::BufRead;
 use std::path::Path;
+use std::str;
+use strum_macros::{Display, EnumString};
 
 #[derive(Parser)]
 #[command(author, version, about)]
 struct Args {
     #[arg(long, default_value_t = String::from("input.txt"))]
     filename: String,
+}
+
+#[derive(Clone, Debug, Display, EnumString, PartialEq)]
+enum Entry {
+    Val(i32),
+    List(Vec<Entry>),
 }
 
 fn main() -> Result<()> {
@@ -23,7 +30,128 @@ fn main() -> Result<()> {
     let file = File::open(filename)?;
     let lines: Vec<String> = io::BufReader::new(file).lines().flatten().collect();
 
-    for (line_num, line) in lines.iter().enumerate() {}
+    let mut entries = Vec::new();
+    for line in lines {
+        if line.len() == 0 {
+            continue;
+        }
 
+        let b = line.as_bytes();
+
+        // Line always starts as a list
+        let mut c: usize = 1;
+        entries.push(parse_list(b, &mut c));
+    }
+    for e in &entries {
+        println!("{e:?}");
+    }
+
+    let mut pos = 0;
+    let mut good = Vec::new();
+    loop {
+        if pos >= entries.len() {
+            break;
+        }
+        if compare(&entries[pos], &entries[pos + 1]) == Ordering::Less {
+            good.push(pos / 2 + 1);
+        }
+        pos += 2;
+    }
+    println!("good - {good:?}");
+    println!("sum - {}", good.iter().sum::<usize>());
     Ok(())
+}
+
+fn compare(entry1: &Entry, entry2: &Entry) -> Ordering {
+    match (entry1, entry2) {
+        (Entry::Val(_), Entry::Val(_)) => {
+            panic!("can't get here");
+        }
+        (Entry::Val(_), Entry::List(_)) => {
+            println!("left val");
+            return compare(&Entry::List(vec![entry1.clone()]), entry2);
+        }
+        (Entry::List(_), Entry::Val(_)) => {
+            println!("right val");
+            return compare(entry1, &Entry::List(vec![entry2.clone()]));
+        }
+        (Entry::List(a), Entry::List(b)) => {
+            println!("list a: {a:?}");
+            println!("list b: {b:?}");
+            let mut aa = a.iter();
+            let mut bb = b.iter();
+
+            if a.len() == 0 && b.len() == 0 {
+                return Ordering::Equal;
+            }
+            loop {
+                let nexta = aa.next();
+                let nextb = bb.next();
+                // If they both go empty together this is equality, not left running out first.
+                if nexta.is_none() && nextb.is_none() {
+                    return Ordering::Equal;
+                }
+                // Left side running out is fine.
+                if nexta.is_none() {
+                    return Ordering::Less;
+                }
+                // 2nd one running out is bad.
+                if nextb.is_none() {
+                    return Ordering::Greater;
+                }
+                let nexta = nexta.unwrap();
+                let nextb = nextb.unwrap();
+                if let (Entry::Val(compa), Entry::Val(compb)) = (nexta, nextb) {
+                    if compa > compb {
+                        println!("{compa} > {compb}");
+                        return Ordering::Greater;
+                    }
+                    if compa < compb {
+                        return Ordering::Less;
+                    }
+                } else {
+                    let ret = compare(nexta, nextb);
+                    if ret != Ordering::Equal {
+                        return ret;
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn parse_list(b: &[u8], c: &mut usize) -> Entry {
+    let mut entry = Vec::new();
+    let mut start = None;
+    loop {
+        if *c >= b.len() {
+            break;
+        }
+        match b[*c] {
+            b'[' => {
+                *c += 1;
+                let e = parse_list(b, c);
+                entry.push(e);
+            }
+            // At the top level comma's are just skipped.
+            b',' | b']' => {
+                if let Some(s) = start {
+                    let sub = str::from_utf8(&b[s..*c]).unwrap();
+                    let val = i32::from_str_radix(sub, 10).unwrap();
+                    entry.push(Entry::Val(val));
+                    start = None;
+                }
+                if b[*c] == b']' {
+                    break;
+                }
+            }
+            _ => {
+                if start.is_none() {
+                    start = Some(*c);
+                }
+            }
+        }
+        *c += 1;
+    }
+    Entry::List(entry)
 }
