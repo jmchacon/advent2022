b71626f44d2baf7bc5f12ca31eccc2f1b52f2016
James Chacon <chacon.james@gmail.com>
2022-12-15 07:51:11 +0000

    Part2
    Find the first non-covered item within a bounding box.
    
    It's...big. 4Mx4M so even a naive "we'll just iterate 4M rows" is way too slow. 2/sec on a single core so even getting to 24 cores would still take a day.
    
    Realized all we care is the interval covered on a line. Once we find the first space not covered we're good.
    
    Wrote a sliding interval detector and gets done in <12s.

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -1,6 +1,7 @@
 //! day15 advent 2022
 use clap::Parser;
 use color_eyre::eyre::Result;
+use std::collections::HashMap;
 use std::fs::File;
 use std::io;
 use std::io::BufRead;
@@ -14,10 +15,16 @@ struct Args {
 
     #[arg(long, default_value_t = 2000000)]
     target: i64,
+
+    #[arg(long, default_value_t = 0)]
+    boundingx: i64,
+
+    #[arg(long, default_value_t = 4000000)]
+    boundingy: i64,
 }
 
 #[derive(Clone, Debug, Eq, Hash, PartialEq)]
-struct Ent(i64, i64, u64);
+struct Ent(i64, i64);
 
 fn main() -> Result<()> {
     color_eyre::install()?;
@@ -28,7 +35,7 @@ fn main() -> Result<()> {
     let lines: Vec<String> = io::BufReader::new(file).lines().flatten().collect();
 
     let (mut minx, mut maxx, mut miny, mut maxy) = (i64::MAX, i64::MIN, i64::MAX, i64::MIN);
-    let mut inp = Vec::new();
+    let mut inp = HashMap::new();
     for (line_num, line) in lines.iter().enumerate() {
         let parts = line.split_whitespace().collect::<Vec<_>>();
         assert!(parts.len() == 10, "{} - bad line {line}", line_num + 1);
@@ -58,8 +65,8 @@ fn main() -> Result<()> {
         min_check(by, &mut miny, 0);
         max_check(y, &mut maxy, dist);
         max_check(by, &mut maxy, 0);
-        inp.push(Ent(x, y, dist));
-        inp.push(Ent(bx, by, 0));
+        inp.insert(Ent(x, y), dist);
+        inp.insert(Ent(bx, by), 0);
     }
 
     let width = maxx - minx + 1;
@@ -71,33 +78,100 @@ fn main() -> Result<()> {
 
     // Find all covered spaces on row args.target and then subtract off any beacons/sensors on that line.
     let mut covered = vec![false; width as usize];
-    for i in &inp {
-        // we now skip beacons.
-        if i.2 == 0 {
-            continue;
-        }
-        let dist = i.1.abs_diff(args.target);
-        if dist <= i.2 {
-            let tot = ((i.2 * 2 + 1) - dist * 2) as usize;
-            let start = ((i.0 - (tot / 2) as i64) - minx) as usize;
-            for i in start..start + tot {
-                covered[i] = true;
-            }
-        }
-    }
-    for i in &inp {
-        // See if it's a beacon/sensor on this row. That can't be a beacon then.
-        if i.1 == args.target {
-            covered[(i.1 - minx) as usize] = false;
-        }
-    }
+    cover_row(&mut covered, &inp, args.target, minx);
     let sum = covered
         .iter()
         .map(|v| if *v { 1 } else { 0 })
         .sum::<usize>();
     println!("sum - {sum}");
-    //println!("{covered:?}");
+
+    for y in args.boundingx..=args.boundingy {
+        /*if y % 1000 == 0 {
+            println!("{y}");
+        }*/
+        if cover2_row(&inp, y, args.boundingx) {
+            break;
+        }
+        /*
+        for (pos, c) in covered.iter().enumerate() {
+            let p = pos as i64;
+            // Skip if outside the bounding box
+            if p + minx < args.boundingx || p + minx > args.boundingy {
+                continue;
+            }
+            if !c && !inp.contains_key(&Ent(p + minx, y)) {
+                println!("{},{y}", p + minx);
+                println!("freq = {}", (p + minx) * 4000000 + y);
+                done = true;
+                break;
+            }
+        }*/
+    }
     Ok(())
+}
+
+fn cover_row(covered: &mut Vec<bool>, inp: &HashMap<Ent, u64>, target: i64, minx: i64) {
+    for (k, v) in inp {
+        // we now skip beacons.
+        if *v == 0 {
+            continue;
+        }
+        let dist = k.1.abs_diff(target);
+        if dist <= *v {
+            let tot = ((*v * 2 + 1) - dist * 2) as usize;
+            let start = ((k.0 - (tot / 2) as i64) - minx) as usize;
+            for s in start..start + tot {
+                covered[s] = true;
+            }
+        }
+    }
+    for (k, _) in inp {
+        // See if it's a beacon/sensor on this row. That can't be a beacon then.
+        if k.1 == target {
+            covered[(k.0 - minx) as usize] = false;
+        }
+    }
+}
+
+#[derive(Clone, Debug, Eq, Hash, PartialEq, PartialOrd, Ord)]
+struct Interval(i64, i64);
+
+fn cover2_row(inp: &HashMap<Ent, u64>, target: i64, start_min: i64) -> bool {
+    let mut ints = Vec::new();
+    for (k, v) in inp {
+        // we now skip beacons.
+        if *v == 0 {
+            continue;
+        }
+        let dist = k.1.abs_diff(target);
+        if dist <= *v {
+            let tot = ((*v * 2 + 1) - dist * 2) as i64;
+            let start = k.0 - (tot / 2) as i64;
+            ints.push(Interval(start, tot));
+        }
+    }
+    ints.sort();
+    let mut min = start_min;
+    for i in ints {
+        // Skip if outside our box/overlap with an existing span.
+        if i.0 + i.1 < min {
+            continue;
+        }
+        if i.0 > min {
+            if !inp.contains_key(&Ent(min, target)) {
+                println!("{},{target}", min);
+                println!("freq = {}", min * 4000000 + target);
+                return true;
+            }
+        }
+
+        if i.0 < min {
+            min = (min - (min - i.0)) + i.1;
+        } else {
+            min += i.1;
+        }
+    }
+    false
 }
 
 fn min_check(a: i64, min: &mut i64, dist: u64) {
