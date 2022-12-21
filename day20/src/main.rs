8737fa064ba18ebcd96865fe5183e5e9e6776b31
James Chacon <chacon.james@gmail.com>
2022-12-20 23:44:01 -0800

    day 20 part 1 and 2.
    part1 I was off by one because indexing at 0 base means the mod math needs to also drop into that range. i.e. len 5000 = mod 4999 for 0 based indexes. Original impl was off and had to get a hint.
    
    2nd part is trivial. Mulitiply and run N times.

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -1,8 +1,6 @@
 //! day20 advent 2022
 use clap::Parser;
 use color_eyre::eyre::Result;
-use slab_tree::tree::TreeBuilder;
-use std::collections::HashMap;
 use std::fs::File;
 use std::io;
 use std::io::BufRead;
@@ -13,6 +11,12 @@ use std::path::Path;
 struct Args {
     #[arg(long, default_value_t = String::from("input.txt"))]
     filename: String,
+
+    #[arg(long, default_value_t = 1)]
+    scale: i64,
+
+    #[arg(long, default_value_t = 1)]
+    rounds: usize,
 }
 
 fn main() -> Result<()> {
@@ -23,7 +27,88 @@ fn main() -> Result<()> {
     let file = File::open(filename)?;
     let lines: Vec<String> = io::BufReader::new(file).lines().flatten().collect();
 
-    for (line_num, line) in lines.iter().enumerate() {}
+    let mut input = Vec::new();
+    for line in &lines {
+        //println!("{line}");
+        let val = i64::from_str_radix(line, 10).unwrap();
+        input.push(val * args.scale);
+    }
+    let mut indexes = Vec::new();
+    for i in 0..input.len() {
+        indexes.push(i);
+    }
+    println!("{input:?}");
+    // We're modding indexes which are 0 based, not 1 so subtract 1.
+    let len = indexes.len() as i64 - 1;
+    for _ in 0..args.rounds {
+        for (pos, i) in input.iter().enumerate() {
+            let i = i; // % len;
 
+            let cur = indexes[pos];
+
+            if *i == 0 {
+                continue;
+            }
+
+            let mut new = cur as i64 + i;
+            // This mess deals with going negative.
+            // Move into the right range then add and mod off to get positive if needed.
+            new = ((new % len) + len) % len;
+
+            //println!("Moving {i} to {new}");
+            indexes[pos] = new as usize;
+
+            for i in 0..indexes.len() {
+                // If it moved right
+                if i != pos && indexes[i] >= cur && indexes[i] <= new as usize {
+                    /*println!(
+                        "{i},{pos} Shifting {} from {} to {}",
+                        input[i],
+                        indexes[i],
+                        indexes[i] - 1
+                    );*/
+                    indexes[i] -= 1;
+                    continue;
+                }
+                if i != pos && indexes[i] >= new as usize && indexes[i] < cur {
+                    /*println!(
+                        "{i},{pos} Shifting {} from {} to {}",
+                        input[i],
+                        indexes[i],
+                        indexes[i] + 1
+                    );*/
+                    indexes[i] += 1;
+                }
+            }
+            //print_indexes(&input, &indexes);
+        }
+    }
+    let f = print_indexes(&input, &indexes);
+    let mut pos = 0;
+    for i in &input {
+        if *i == 0 {
+            break;
+        }
+        pos += 1;
+    }
+    let zero = indexes[pos];
+    let thousand = f[(zero + 1000) % indexes.len()];
+    let two_thousand = f[(zero + 2000) % indexes.len()];
+    let three_thousand = f[(zero + 3000) % indexes.len()];
+    println!("0 at index {}", zero);
+    println!("1000 is {}", thousand);
+    println!("2000 is {}", two_thousand);
+    println!("3000 is {}", three_thousand);
+    println!("sum is {}", thousand + two_thousand + three_thousand);
     Ok(())
+}
+
+fn print_indexes(input: &Vec<i64>, indexes: &Vec<usize>) -> Vec<i64> {
+    let mut fixed = Vec::new();
+    fixed.resize(indexes.len(), 0);
+    for (pos, i) in indexes.iter().enumerate() {
+        fixed[*i] = input[pos];
+    }
+    println!("{fixed:?}");
+    fixed
 }
