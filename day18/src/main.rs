d1870e63d7e369e06afdfb759e222294459acd5e
James Chacon <chacon.james@gmail.com>
2022-12-22 12:17:39 -0800

    day 18 part 2
    first attempt. it's wrong

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -1,6 +1,7 @@
 //! day18 advent 2022
 use clap::Parser;
 use color_eyre::eyre::Result;
+use std::collections::HashSet;
 use std::fs::File;
 use std::io;
 use std::io::BufRead;
@@ -34,15 +35,61 @@ fn main() -> Result<()> {
             i64::from_str_radix(parts[2], 10).unwrap(),
         ))
     }
+    let mut entries = HashSet::new();
+    squares.iter().for_each(|v| {
+        entries.insert(v);
+    });
     // If nothing touches we get 6 faces per square.
+    let (mut maxx, mut maxy, mut maxz) = (i64::MIN, i64::MIN, i64::MIN);
+    let (mut minx, mut miny, mut minz) = (i64::MAX, i64::MAX, i64::MAX);
     let mut faces = squares.len() * 6;
     for i in 0..squares.len() {
         let compare = &squares[i];
+        if compare.0 > maxx {
+            maxx = compare.0
+        }
+        if compare.1 > maxy {
+            maxy = compare.1
+        }
+        if compare.2 > maxz {
+            maxz = compare.2
+        }
+        if compare.0 < minx {
+            minx = compare.0
+        }
+        if compare.1 < miny {
+            miny = compare.1
+        }
+        if compare.2 < minz {
+            minz = compare.2
+        }
         for j in i + 1..squares.len() {
             faces -= touching(compare, &squares[j]);
         }
     }
+    let mut faces2 = faces;
+    for x in minx..=maxx {
+        for y in miny..=maxy {
+            for z in minz..=maxz {
+                let t = Location(x, y, z);
+                if !entries.contains(&t) {
+                    let mut sum = 0;
+                    for compare in &squares {
+                        sum += touching(&t, compare);
+                    }
+                    if sum == 12 {
+                        faces2 -= 6;
+                        println!("Trying {t:?} = {}", sum);
+                    }
+                }
+            }
+        }
+    }
+    println!("min,max x {minx},{maxx}");
+    println!("min,max y {miny},{maxy}");
+    println!("min,max z {minz},{maxz}");
     println!("{faces}");
+    println!("{faces2}");
 
     Ok(())
 }
