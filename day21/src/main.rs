3cb135f0763d474d49eec36a09ce1af412e97733
James Chacon <chacon.james@gmail.com>
2023-01-05 14:50:33 -0800

    day21 part2
    Need to figure out what value to make humn so it matches the other subtree from root.
    
    Do this by adding an operation state Unknown and setting humn to this. Then refuse to compute operations we don't know so we end up with one branch from root which isn't computed.
    
    Find our match value as the subtree from root which has a value.
    
    Then recurse through the other tree inverting each op against the current value.
    
    i.e. initial value is 150 and first op is "divide by 4" which means "multiple by 4" to invert it.
    
    Then go through each layer doing this until we run out of nodes and try to process humn. At that point we know the algebra answer for this.

--- day21/src/main.rs
+++ day21/src/main.rs
@@ -22,18 +22,25 @@ enum Operation {
     Minus,
     Multiply,
     Divide,
+    Unknown,
 }
 
 #[derive(Clone, Debug, Display, Eq, Hash, PartialEq)]
-enum Entry {
+enum Item {
     Value(i64),
     Operation(Operation),
+}
+
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+struct Entry<'a> {
+    name: &'a str,
+    value: Item,
 }
 
 #[derive(Clone, Debug)]
 struct Definition<'a> {
     children: Vec<&'a str>,
-    op: Entry,
+    op: Entry<'a>,
 }
 
 fn main() -> Result<()> {
@@ -56,7 +63,10 @@ fn main() -> Result<()> {
                     key,
                     Definition {
                         children: vec![],
-                        op: Entry::Value(val),
+                        op: Entry {
+                            name: key,
+                            value: Item::Value(val),
+                        },
                     },
                 );
             }
@@ -66,21 +76,20 @@ fn main() -> Result<()> {
                     "-" => Operation::Minus,
                     "*" => Operation::Multiply,
                     "/" => Operation::Divide,
-                    _ => {
-                        panic!("{} - bad line {line}", line_num + 1);
-                    }
+                    _ => panic!("{} - bad line {line}", line_num + 1),
                 };
                 hm.insert(
                     key,
                     Definition {
                         children: vec![parts[1], parts[3]],
-                        op: Entry::Operation(op),
+                        op: Entry {
+                            name: key,
+                            value: Item::Operation(op),
+                        },
                     },
                 );
             }
-            _ => {
-                panic!("{} - bad line {line}", line_num + 1);
-            }
+            _ => panic!("{} - bad line {line}", line_num + 1),
         }
     }
 
@@ -88,6 +97,134 @@ fn main() -> Result<()> {
         println!("{k} -> {v:?}");
     }
 
+    let mut tree = make_tree(&hm);
+
+    let mut s = String::new();
+    tree.write_formatted(&mut s).unwrap();
+    println!("{s}");
+
+    transform_tree(&mut tree, false);
+
+    s.clear();
+    tree.write_formatted(&mut s).unwrap();
+    println!("{s}");
+
+    println!("root is {:?}", tree.root().unwrap().data());
+
+    let mut tree = make_tree(&hm);
+    transform_tree(&mut tree, true);
+
+    s.clear();
+    tree.write_formatted(&mut s).unwrap();
+    println!("{s}");
+
+    let mut cur = 0;
+    let mut node = tree.root().unwrap();
+    for c in tree.root().unwrap().children() {
+        if let Item::Value(v) = c.data().value {
+            // Normally not good but we know the input data fits
+            cur = v;
+            continue;
+        }
+        if let Item::Operation(_) = c.data().value {
+            node = c;
+        }
+    }
+    println!("{:?} = {cur}", node.data());
+    let mut ops = 0;
+    loop {
+        // Each step is
+        //
+        // cur = x op value
+        //
+        // or
+        //
+        // cur = value op x
+        //
+        // Invert that so cur becomes a new value based on inverse of op and value
+        // and set child to the node which is the op.
+        // Stop when we find the unknown node and report cur.
+        let Item::Operation(op) = node.data().value.clone() else {
+            panic!();
+        };
+
+        // This is the humn node so we're done.
+        if op == Operation::Unknown {
+            break;
+        }
+        ops += 1;
+        let children = node.children().collect::<Vec<_>>();
+        let v = if let Item::Value(v) = children[0].data().value {
+            v
+        } else if let Item::Value(v) = children[1].data().value {
+            v
+        } else {
+            panic!();
+        };
+        let idx: usize = if let Item::Operation(_) = children[0].data().value {
+            0
+        } else {
+            1
+        };
+        let child1_op = if let Item::Operation(_) = children[0].data().value {
+            false
+        } else {
+            true
+        };
+        println!("{op} {v} - {cur}");
+        match op {
+            Operation::Plus => {
+                // order here doesn't matter.
+                //
+                // cur = x + value
+                // x = cur - value
+                cur -= v;
+            }
+            Operation::Minus => {
+                // order here does matter.
+                //
+                // cur = x - value
+                // x = cur + value
+                //
+                // cur = value - x
+                // x = value - cur;
+                if child1_op {
+                    cur += v;
+                } else {
+                    cur = v - cur;
+                }
+            }
+            Operation::Multiply => {
+                // order here doesn't matter.
+                //
+                // cur = x * value
+                // x = cur / value
+                cur /= v;
+            }
+            Operation::Divide => {
+                // order here does matter.
+                //
+                // cur = x / value
+                // x = cur * value
+                //
+                // cur = value / x
+                // x = value / cur
+                if child1_op {
+                    cur *= v;
+                } else {
+                    cur = v / cur;
+                }
+            }
+            Operation::Unknown => panic!(),
+        };
+        println!("{cur}");
+        node = tree.get(children[idx].node_id()).unwrap();
+    }
+    println!("x = {cur} in {ops} ops");
+    Ok(())
+}
+
+fn make_tree<'a>(hm: &'a HashMap<&str, Definition>) -> Tree<Entry<'a>> {
     let mut tree: Tree<Entry> = Tree::new();
     let mut work = Vec::new();
     tree.set_root(hm.get(&"root").unwrap().op.clone());
@@ -101,23 +238,30 @@ fn main() -> Result<()> {
         //println!("processing {e:?}");
         let op = hm.get(e.1).unwrap();
         let mut n = tree.get_mut(e.0).unwrap();
-        match &op.op {
-            Entry::Value(v) => {
-                n.append(Entry::Value(*v));
+        match &op.op.value {
+            Item::Value(v) => {
+                n.append(Entry {
+                    name: op.op.name,
+                    value: Item::Value(*v),
+                });
             }
-            Entry::Operation(o) => {
-                let id = n.append(Entry::Operation(o.clone())).node_id();
+            Item::Operation(o) => {
+                let id = n
+                    .append(Entry {
+                        name: op.op.name,
+                        value: Item::Operation(o.clone()),
+                    })
+                    .node_id();
                 // This pushes the children in reverse order. Account below when we do the op.
                 work.push((id, op.children[0]));
                 work.push((id, op.children[1]));
             }
         }
     }
+    tree
+}
 
-    let mut s = String::new();
-    tree.write_formatted(&mut s).unwrap();
-    println!("{s}");
-
+fn transform_tree(tree: &mut Tree<Entry>, part2: bool) {
     for n in tree
         .root()
         .unwrap()
@@ -125,37 +269,51 @@ fn main() -> Result<()> {
         .map(|n| n.node_id())
         .collect::<Vec<_>>()
     {
+        if part2 {
+            let mut skip = false;
+            for c in tree.get(n).unwrap().children() {
+                if c.data().name == "humn" {
+                    let humn = c.node_id();
+                    let mut node = tree.get_mut(humn).unwrap();
+                    node.data().value = Item::Operation(Operation::Unknown);
+                    skip = true;
+                    break;
+                }
+                if let Item::Operation(_) = c.data().value {
+                    skip = true;
+                    break;
+                }
+            }
+            if skip {
+                continue;
+            }
+        }
         let node = tree.get(n).unwrap();
         // Values are end nodes so we don't care here.
-        if let Entry::Value(_) = node.data() {
+        if let Item::Value(_) = node.data().value {
             continue;
         }
-        let Entry::Operation(op) = node.data().clone() else {
+        let Item::Operation(op) = node.data().value.clone() else {
             panic!();
         };
         let vals = node
             .children()
-            .map(|v| match v.data() {
-                Entry::Value(v) => *v,
-                Entry::Operation(_) => {
+            .map(|v| match v.data().value {
+                Item::Value(v) => v,
+                Item::Operation(_) => {
                     panic!()
                 }
             })
             .collect::<Vec<_>>();
+        //println!("Working on {op:?} for {vals:?}");
         let new: i64 = match op {
             Operation::Plus => vals.iter().sum(),
             Operation::Minus => vals[1] - vals[0],
             Operation::Multiply => vals[1] * vals[0],
             Operation::Divide => vals[1] / vals[0],
+            Operation::Unknown => panic!(),
         };
         let mut node = tree.get_mut(n).unwrap();
-        *node.data() = Entry::Value(new);
+        node.data().value = Item::Value(new);
     }
-
-    s.clear();
-    tree.write_formatted(&mut s).unwrap();
-    println!("{s}");
-
-    println!("root is {:?}", tree.root().unwrap().data());
-    Ok(())
 }
